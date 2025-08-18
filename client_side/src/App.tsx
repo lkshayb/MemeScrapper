09e45cebfabfff501593750ce7559ca3dee976fd
Lakshay Bhatia <b.lakshay.aspect@gmail.com>
2025-08-18 17:42:05 +0530

    First commit

---
+++ client_side/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Header from "./ui/comp/Header.tsx"
+function App() {
+  
+  return (
+    <div className='bg-slate-200'>
+      <Header/>
+    </div>
+  )
+}
+
+export default App
