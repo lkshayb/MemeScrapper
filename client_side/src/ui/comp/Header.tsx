7fdbcf0104a2a5aaa8e733b9dead039e6d2a58de
Lakshay Bhatia <b.lakshay.aspect@gmail.com>
2025-08-18 21:45:50 +0530

    Updated frontend and added separate dirs

--- client_side/src/ui/comp/Header.tsx
+++ client_side/src/ui/comp/Header.tsx
@@ -1,7 +1,14 @@
 export default function Header(){
     return(
-      <div>
-        hello
+      <div className="flex items-center justify-between font-[Work Sans] text-white bg-slate-950 border-b-1 border-white/10 py-4 px-20">
+        <div className="font-[LimeLight] font-bold text-xl">
+          MemeScrapper
+        </div>
+        <div className="flex text-slate-400 font-medium  gap-10 ">
+          <span className="hover:text-slate-200 hover:cursor-pointer duration-300 py-1">Home</span>
+          <span className="hover:text-slate-200 hover:cursor-pointer duration-300 py-1">About</span>
+          <span className="hover:text-slate-200 hover:cursor-pointer duration-300 rounded py-1 px-3 text-white/70 bg-slate-800">Log in</span>
+        </div>
       </div>
     )
 }
